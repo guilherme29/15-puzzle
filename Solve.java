c709a15b2220b80b2a9e91844f43c1bdf35781ff
Guilherme M <gui.amado29@gmail.com>
2018-03-04 17:01:31 +0000

    função para calcular o número de inversões numa dada configuração passada como vetor

--- Solve.java
+++ Solve.java
@@ -1,8 +1,20 @@
 
 
-class Solve{
-    public static boolean Solvability(
-    Node source, Node objective, int height, int width){
-        
+abstract class Solve{
+    abstract boolean Solvability(
+    Node source, Node objective, int height, int width);
+
+    public static int inversion(int[] source){
+        int inv_count = 0;
+        for(int i=0;i<source.length;i++){//corremos o vetor source e comparamos todos os pares
+            for(int j=i+1;j<source.length;j++){
+                if(source[i] > source[j] && source[j]!=0){
+                    inv_count++;
+                }
+            }
+        }
+        return inv_count;
     }
+
+    
 }
