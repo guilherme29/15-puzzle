bab07022f14ec8daf7d71c96c37497bbb46177f5
XxArcaiCxX <peuporsche@hotmail.com>
2018-03-04 20:11:49 +0000

    Resolvido problema de output

--- Puzzle.java
+++ Puzzle.java
@@ -46,38 +46,38 @@ class Puzzle{
 
       switch(modes){
         case 1: Solve.hasSolution(beta, alfa, height, width);
-                if(Solve.Solvability(beta, alfa, height, width)!= -1 || Solve.Solvability(beta, alfa, height, width) != 0){
+                if(Solve.Solvability(beta, alfa, height, width)!= -1 && Solve.Solvability(beta, alfa, height, width) != 0){
                   System.out.println("Resposta passo a passo:");
                   Algorithms.dfs(beta, alfa, height, width);
                 }
                 break;
         case 2: Solve.hasSolution(beta, alfa, height, width);
-                if(Solve.Solvability(beta, alfa, height, width)!= -1 || Solve.Solvability(beta, alfa, height, width) != 0){
+                if(Solve.Solvability(beta, alfa, height, width)!= -1 && Solve.Solvability(beta, alfa, height, width) != 0){
                   System.out.println("Resposta passo a passo:");
                   Algorithms.bfs(beta, alfa, height, width);
                 }
                 break;
         case 3: Solve.hasSolution(beta, alfa, height, width);
-                if(Solve.Solvability(beta, alfa, height, width)!= -1 || Solve.Solvability(beta, alfa, height, width) != 0){
+                if(Solve.Solvability(beta, alfa, height, width)!= -1 && Solve.Solvability(beta, alfa, height, width) != 0){
                   System.out.println("Tem Solucao");
                   System.out.println("Resposta passo a passo:");
                   Algorithms.ldfs_print(beta, alfa, height, width, maxdepth);
                 }
                 break;
         case 4: Solve.hasSolution(beta, alfa, height, width);
-                if(Solve.Solvability(beta, alfa, height, width)!= -1 || Solve.Solvability(beta, alfa, height, width) != 0){
+                if(Solve.Solvability(beta, alfa, height, width)!= -1 && Solve.Solvability(beta, alfa, height, width) != 0){
                   System.out.println("Resposta passo a passo:");
                   Algorithms.idfs(beta, alfa, height, width);
                 }
                 break;
         case 5: Solve.hasSolution(beta, alfa, height, width);
-                if(Solve.Solvability(beta, alfa, height, width)!= -1 || Solve.Solvability(beta, alfa, height, width) != 0){
+                if(Solve.Solvability(beta, alfa, height, width)!= -1 && Solve.Solvability(beta, alfa, height, width) != 0){
                   System.out.println("Resposta passo a passo:");
                   Algorithms.a_star(beta, alfa, height, width);
                 }
                 break;
         case 6: Solve.hasSolution(beta, alfa, height, width);
-                if(Solve.Solvability(beta, alfa, height, width)!= -1 || Solve.Solvability(beta, alfa, height, width) != 0){
+                if(Solve.Solvability(beta, alfa, height, width)!= -1 && Solve.Solvability(beta, alfa, height, width) != 0){
                   System.out.println("Resposta passo a passo:");
                   Algorithms.greedy(beta, alfa, height, width);
                 }
